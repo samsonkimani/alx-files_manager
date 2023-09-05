54b53336480800154568fccafcc446d44e65fe7c
samsonkimani <samsonkimani43@gmail.com>
2023-09-05 17:11:24 +0300

    fixing changes in db connection

--- utils/db.js
+++ utils/db.js
@@ -1,37 +1,39 @@
 import pkg from 'mongodb';
+
 const { MongoClient } = pkg;
 
 class DBClient {
-	constructor () {
-		const dbHost = process.env.DB_HOST || 'localhost';
-		const dbPort = process.env.DB_PORT || 27017;
-		const dbName = process.env.DB_DATABASE || 'files_manager';
+  constructor() {
+    const dbHost = process.env.DB_HOST || 'localhost';
+    const dbPort = process.env.DB_PORT || 27017;
+    const dbName = process.env.DB_DATABASE || 'files_manager';
 
-		const url = `mongodb: //${dbHost}:${dbPort}/${dbName}`;
-		this.client = new MongoClient(url, {
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		});
+    const url = `mongodb://${dbHost}:${dbPort}/${dbName}`;
+    this.client = new MongoClient(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-		this.client.on('connect', (error) => {
-			if (error) {
-				console.log('Mongodb connection error', error);
-			}
-		});
+    this.client.connect((error) => {
+      if (error) {
+        console.log('Mongodb connection error', error);
+      }
+    });
+  }
 
-	}
+  isAlive() {
+    return this.client.isConnected();
+  }
 
-	isAlive() {
-		return this.client.isConnected();
-	}
-	async nbUsers() {
-		const nbUserCollections = await this.client.db().collection('users');
-		return nbUserCollections.countDocuments();
-	}
-	async nbFiles() {
-                const filesCollections = await this.client.db().collection('files');
-                return filesCollections.countDocuments();
-        }
+  async nbUsers() {
+    const nbUserCollections = await this.client.db().collection('users');
+    return nbUserCollections.countDocuments();
+  }
+
+  async nbFiles() {
+    const filesCollections = await this.client.db().collection('files');
+    return filesCollections.countDocuments();
+  }
 }
 const dbClient = new DBClient();
 export default dbClient;
